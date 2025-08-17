aed1ae9fb2507b195efd188fb884693187b3b439
Davis Guest <davisdevice@gmail.com>
2025-08-16 21:45:00 -0400

    metadata update

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,32 @@ import Header from "./components/header";
 
 export const metadata = {
   title: "Thomas Guest Music",
-  description: "Official website for my music, events, and updates",
+  description: "Official website for Thomas Guest Music. Explore new song releases, events, and updates.",
+  keywords: ["Thomas Guest", "Music", "Singer", "Country Music", "Music"],
+  authors: [{ name: "Thomas Guest" }],
+
+  openGraph: {
+    title: "Thomas Guest Music",
+    description: "Official website for Thomas Guest Music. Explore new song releases, events, and updates.",
+    url: "https://tg-music-seven.vercel.app/",
+    siteName: "Thomas Guest Music",
+    type: "website",
+    images: [
+      {
+        url: "https://tg-music-seven.vercel.app/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Thomas Guest Music",
+      },
+    ],
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Thomas Guest Music",
+    description: "Official website for Thomas Guest Music. Explore new song releases, events, and updates.",
+    images: ["https://tg-music-seven.vercel.app/og-image.jpg"],
+  },
 };
 
 export default function RootLayout({ children }: Readonly <{ children: React.ReactNode }>) {
