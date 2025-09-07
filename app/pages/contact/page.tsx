539c2b9d0005e403e0648bbe61312131452525b2
Davis Guest <davisdevice@gmail.com>
2025-09-07 18:49:07 -0400

    set background for contact page

--- app/pages/contact/page.tsx
+++ app/pages/contact/page.tsx
@@ -1,6 +1,23 @@
+"use client";
+import { useEffect } from "react";
 import * as Globals from "@/app/globals";
 
 export default function ContactPage() {
+
+     useEffect(() => {
+          document.body.style.backgroundImage = "url('/black-static.png')";
+          document.body.style.backgroundSize = "cover";
+          document.body.style.backgroundPosition = "center";
+          document.body.style.backgroundRepeat = "no-repeat";
+      
+          return () => {
+            document.body.style.backgroundImage = "";
+            document.body.style.backgroundSize = "";
+            document.body.style.backgroundPosition = "";
+            document.body.style.backgroundRepeat = "";
+          };
+        }, []);
+
      return (
           <main>
                <div className="contact">
