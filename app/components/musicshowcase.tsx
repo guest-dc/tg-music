1fd6aff4ea998b497afe1848e7d14ce52eebf65a
Davis Guest <davisdevice@gmail.com>
2026-06-19 17:29:23 -0400

    kicking stones update

--- app/components/musicshowcase.tsx
+++ app/components/musicshowcase.tsx
@@ -6,11 +6,18 @@ export default function MusicShowcase() {
 
      <div className="tracks-grid">
 
-        <div key="findyou" className="track-item">
-            <a href="https://open.spotify.com/track/2mExeMPZel1WrFQ2p9rWur?si=0044125b3e354020" target="_blank" rel="noopener noreferrer">
-              <img src="/musicThumbnails/findyou.jpg" alt="spotify-findyou" />
+        <div key="kickingstones" className="track-item">
+            <a href="https://open.spotify.com/track/0PZ2I2VLmdeJJ02FBTbXGM?si=0094c8979fde4c0b" target="_blank" rel="noopener noreferrer">
+              <img src="/musicThumbnails/kickingstones.jpg" alt="spotify-kickingstones" />
             </a>
             <p>Find You</p>
+        </div>
+
+        <div key="mysterygirl" className="track-item">
+          <a href="https://open.spotify.com/track/3DdyYwJPBHwGtxX2ziepBQ?si=cc00bf87b93b47d0" target="_blank" rel="noopener noreferrer">
+            <img src="/musicThumbnails/mysterygirl.jpg" alt="spotify-mysterygirl" />
+          </a>
+          <p>Mystery Girl</p>
         </div>
 
         <div key="makingchangesinmacon" className="track-item">
@@ -20,11 +27,11 @@ export default function MusicShowcase() {
           <p>Making Changes in Macon</p>
         </div>
 
-        <div key="mysterygirl" className="track-item">
-          <a href="https://open.spotify.com/track/3DdyYwJPBHwGtxX2ziepBQ?si=cc00bf87b93b47d0" target="_blank" rel="noopener noreferrer">
-            <img src="/musicThumbnails/mysterygirl.jpg" alt="spotify-mysterygirl" />
-          </a>
-          <p>Mystery Girl</p>
+        <div key="findyou" className="track-item">
+            <a href="https://open.spotify.com/track/2mExeMPZel1WrFQ2p9rWur?si=0044125b3e354020" target="_blank" rel="noopener noreferrer">
+              <img src="/musicThumbnails/findyou.jpg" alt="spotify-findyou" />
+            </a>
+            <p>Find You</p>
         </div>
 
       </div>
